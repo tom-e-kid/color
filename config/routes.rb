fcbfe6b121f60fe5b6f306d4e8f1651c7f87571a
Tomohiko Ikeda <tom_e_kid@mac.com>
2014-11-13 03:13:51 +0900

    Create Root Path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'facade_pages/home'
+
+  root 'facade_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ spec/requests/facade_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "facade pages" do
+	
+	subject { page }
+
+	describe "home" do
+		before { visit root_path }
+
+		it { should have_content('Color') }
+		it { should have_link('Sign up') }
+		it { should have_link('Sign in') }
+	end
+end
