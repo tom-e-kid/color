a5f3cc30e5d2c9960bbaaf245861373e7185b5b2
Tomohiko Ikeda <tom_e_kid@mac.com>
2014-11-17 01:08:03 +0900

    Keep clean

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "user pages" do
 	
   subject { page }
 
-  describe "profile page (show)" do
+  describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
   	before { visit user_path(user) }
 
@@ -12,7 +12,7 @@ describe "user pages" do
 	it { should have_content(user.name) }
   end
 
-  describe "signup page (new)" do
+  describe "signup page" do
 	before { visit signup_path }
 
 	it { should have_title(page_title("Sign up")) }
@@ -20,15 +20,21 @@ describe "user pages" do
 
 	let(:submit) { "Create my account" }
 
-	context "invalid info" do
+	context "with invalid info" do
 	  it "should not create user" do
 	  	expect do
 	  		click_button submit
 	  	end.not_to change(User, :count)
 	  end
+
+	  describe "after submission" do
+	  	before { click_button submit }
+	  	it { should have_title('Sign up') }
+	  	it { should have_content('error') }
+	  end
 	end
 
-	context "valid info" do
+	context "with valid info" do
 	  before do
 	  	fill_in "Username", 	with: "example_user"
 	  	fill_in "Email",		with: "example_user@example.com"
@@ -39,6 +45,13 @@ describe "user pages" do
 	  	expect do
 	  		click_button submit
 	  	end.to change(User, :count).by(1)
+	  end
+
+	  describe "after saving user" do
+	  	before { click_button submit }
+	  	let(:user) { User.find_by(email: "example_user@example.com") }
+	  	it { should have_title(user.name) }
+	  	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 	  end
 	end
   end
